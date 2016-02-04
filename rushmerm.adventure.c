ae0ff1e1be00a99d7679e8298346a295cd5f3ff1
Mark Rushmere <mrushmere@gmail.com>
2016-02-04 13:26:03 -0800

    Got the directory with pid made, and full file paths created

--- rushmerm.adventure.c
+++ rushmerm.adventure.c
@@ -1,65 +1,67 @@
-/************************************************************************************
- * Mark Rushmere
- * CS 372
- * Project 2
- * Decription: This program is a text based adventure game. On startup, it creates
- * several data files that contain the information about each room in the games 
- * "Universe". Each file contains the room/area's name, description, and how it connects
- * to the rooms adjacent to it.  
- * ***********************************************************************************/
-
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
-#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
 
 void createRooms();
 
+
+
 int main() {
 
-	//
-
-	
-
+	createRooms();
 
 	return 0;
-
-
 }
+
 
 void createRooms() {
 
-	// Must have an array of connection
-	// generate random number between 3 and 6
-	// add connections for pairs of rooms 
-	FILE* roomFiles[7];
-	char* names[10];
+	FILE* roomFiles[10];
 
 	// Room types
-	enum roomtype = {start = 'START_ROOM', mid = 'MID_ROOM', end = 'END_ROOM' };
+	enum roomtype {START_ROOM, MID_ROOM, END_ROOM};
+	char* roomtypenames[3] = {"start", "middle", "end"};
 
 	// room names
-	names = {"White House", "Cellar", "Cave", "Troll Room", "Treasure Room", };
+	char* names[10] = {"/White House.txt", "/Cellar.txt", "/Cave.txt", "/Troll Room.txt", "/Treasure Room.txt", "/Room6.txt", "/Room7.txt", "/Room8.txt", "/Room9.txt", "/Room10.txt"};
 
-	// Get the process id
+
+	// Get the process id and make the directory string
 	int processId;
 	processId = getpid();
 	char buffer [10];
 	sprintf(buffer, "%d", processId);
-
-	// Make a string for the directory
-	char dir [30];
-	strcpy(dir, "rushmerm.rooms.");	
+	char dir[40];
+	strcpy(dir, "./rushmerm.rooms.");	
 	strcat(dir, buffer);
- 	 
-	char* path1 = strcat(dir, "/whiteRoom.txt");
-	// First im setting up a whole bunch of file pointers that i'll need later
-	FILE * whiteHouse;
-	whiteHouse = fopen(path1, "w");
-	fprintf(whiteHouse, "Hello");
-	rooms[0] = whiteHouse;
+	printf("directory: %s\n", dir);
+	// Make the directory that all the room files will be in
+	mkdir(dir, 0777);
+
+	// Make a array of string with the full file paths to each 
+	char* filepaths[10];
+	int i;
+	for(i =0; i < 10; i++) {
+		filepaths[i] = (char*) malloc(50);
+	}
+
+	for(i =0; i < 10; i++) {
+		strcpy(filepaths[i], dir);
+		strcat(filepaths[i], names[i]);
+		printf("Full file path: %s\n", filepaths[i]);
+	}
+	// Just checking that the room names and types work
+	printf("This is the first room name: %s\n", names[0]);
+	printf("This is the first room type: %s\n", roomtypenames[0]);
+
+
+
+	for(i =0; i < 10; i++) {
+		free(filepaths[i]);
+	}
 
 
 
