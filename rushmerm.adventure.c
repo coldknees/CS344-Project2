8d8adbe91d7c55a5ff20b31dcffe6c902e61e871
Mark Rushmere <mrushmere@gmail.com>
2016-02-05 01:08:24 -0800

    added random number function for rooms

--- rushmerm.adventure.c
+++ rushmerm.adventure.c
@@ -4,10 +4,13 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+
+
 
 void createRooms();
 
-
+void pickRooms(int array[]);
 
 int main() {
 
@@ -19,6 +22,11 @@ int main() {
 
 void createRooms() {
 
+
+	//seed rand function for later
+	time_t t;
+	srand((unsigned) time(&t));
+
 	FILE* roomFiles[10];
 
 	// Room types
@@ -26,7 +34,13 @@ void createRooms() {
 	char* roomtypenames[3] = {"start", "middle", "end"};
 
 	// room names
-	char* names[10] = {"/White House.txt", "/Cellar.txt", "/Cave.txt", "/Troll Room.txt", "/Treasure Room.txt", "/Room6.txt", "/Room7.txt", "/Room8.txt", "/Room9.txt", "/Room10.txt"};
+	char* names[10] = {"WhiteHouse", "Cellar", "Cave", "TrollRoom", "TreasureRoom", "Room6", "Room7", "Room8", "Room9", "Room10"};
+
+	// Array of 7 differnt rooms to be created
+	int selectedRooms[7] = {12};
+
+
+
 
 
 	// Get the process id and make the directory string
@@ -50,7 +64,9 @@ void createRooms() {
 
 	for(i =0; i < 10; i++) {
 		strcpy(filepaths[i], dir);
+		strcat(filepaths[i], "/");
 		strcat(filepaths[i], names[i]);
+		strcat(filepaths[i], ".txt");
 		printf("Full file path: %s\n", filepaths[i]);
 	}
 	// Just checking that the room names and types work
@@ -58,11 +74,59 @@ void createRooms() {
 	printf("This is the first room type: %s\n", roomtypenames[0]);
 
 
+	FILE* temp;
+
+
+	// Write to the files	
+	for(i =0; i < 10; i++) {
+		int firstTaken = 0;
+		int lastTaken = 0;
+		temp = fopen(filepaths[i], "w");
+		fprintf(temp,"ROOM NAME: %s\n", names[i]);
+		fclose(temp);
+	}
+
+
+
+
+	//random integers for the start and end rooms
+	int first = rand() % 10;
+	int last = 0;
+
+	// Pick rooms
+	pickRooms(selectedRooms);
+	int n;
+	for(n = 0; n < 7; n++){
+		printf("%d", selectedRooms[n]);
+	}
+
 
 	for(i =0; i < 10; i++) {
 		free(filepaths[i]);
 	}
 
 
+
+}
+
+
+// This helper function only serves to pick the 7 random rooms
+
+void pickRooms(int array[]) {
+
+	int i;
+	int j;
+	int randNum;
+	for(i = 0; i < 7; i++) {
+		randNum = rand() % 7;
+		//printf("rand num is: %d\n", randNum);
+		for(j=0; j < 7; j++) {
+			if(randNum == array[j]) {
+				i--;
+				break;
+			}
+		}
+		array[i] = randNum;
+	}
 
 }
