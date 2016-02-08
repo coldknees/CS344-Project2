504d12211476ae889556ea227498e48b8a790c39
Mark Rushmere <mrushmere@gmail.com>
2016-02-08 11:24:52 -0800

    Rooms randomly selected and connected. need to add user interface

--- rushmerm.adventure.c
+++ rushmerm.adventure.c
@@ -11,6 +11,9 @@
 char* filepaths[7];
 char* selectedNames[7];
 int connections[7][7] = {0};
+char* roomtypenames[3] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
+char* names[10] = {"WhiteHouse", "Cellar", "Cave", "TrollRoom", "TreasureRoom", "Room6", "Room7", "Room8", "Room9", "Room10"};
+
 
 
 // Function Prototypes
@@ -19,21 +22,35 @@ void createRooms();
 void createConnections();
 void createTypes();
 
+
 // main function
 
 int main() {
+	int i, j;
 
 	srand(time(NULL));
 
 	createRooms();
 
-	int i;
 	for(i = 0; i < 7; i++) {
 		printf("%d: %s\n", i, selectedNames[i]);
 	}
 
 
 	createConnections();
+
+	printf("\n");
+	for(i = 0; i < 7; i++) {
+		for(j = 0; j < 7; j++) {
+			printf("%d\t", connections[i][j]);
+		}
+		printf("\n");
+		
+	}
+
+
+	createTypes();
+
 	return 0;
 }
 
@@ -44,10 +61,9 @@ void createRooms() {
 
 	// Room types
 	enum roomtype {START_ROOM, MID_ROOM, END_ROOM};
-	char* roomtypenames[3] = {"start", "middle", "end"};
+	
 
 	// room names
-	char* names[10] = {"WhiteHouse", "Cellar", "Cave", "TrollRoom", "TreasureRoom", "Room6", "Room7", "Room8", "Room9", "Room10"};
 
 	// Array of 7 differnt rooms to be created
 	int selectedRooms[7] = {-1};
@@ -56,7 +72,7 @@ void createRooms() {
 	pickRooms(selectedRooms);
 	int n;
 	for(n = 0; n < 7; n++){
-		printf("%d", selectedRooms[n]);
+		printf("%d\n", selectedRooms[n]);
 	}
 
 
@@ -98,9 +114,7 @@ void createRooms() {
 	printf("This is the first room name: %s\n", names[0]);
 	printf("This is the first room type: %s\n", roomtypenames[0]);
 
-
 	FILE* temp;
-
 
 	// Write to the files	
 	for(i =0; i < 7; i++) {
@@ -126,6 +140,9 @@ void createConnections() {
 	// first set up the connections matrix so that every room is connected to itself
 	int i;
 	int j;
+	int k;
+	FILE* temp;
+
 	for(i = 0; i < 7; i++) {
 		connections[i][i] = 1;
 	}
@@ -139,52 +156,72 @@ void createConnections() {
 
 
 
-	printf("1");
+	//printf("1");
 	// Now set the rest of the connections
 	for(i = 0; i < 7; i++) {
-		printf("2");
+		//printf("2");
 		// pick a random number between 3 and 6 to use for the number of connections for that room
 		int random;
 		random = (rand() % 4) + 3;
 
-		//make the connections, append files, and check to see if any connections have already been made
-		FILE* temp;
-		for(j = 0; j < random; j++) {
-			printf("3");
+		//How many connections does that room have;
+		int curConn = 0;
+		for(k = 0; k < 7; k++)  {
+			curConn += connections[i][k];
+		}
+
+		//make the connections, fill the matrix
+		while(curConn <= random) {
+			//printf("3");
 			// generate another random number to pick a different room to connect to
 			int randRoom;
 			randRoom = rand() % 7;
 
-			/***
-			while(connections[i][randRoom] == 1) {
+			while(randRoom == i) {
 				randRoom = rand() % 7;
-
-				printf("4");
 			}
-
-			*****/
-
 
 
 			// update the matrix to show that there is a connection made to the room
 			connections[i][randRoom] = 1;
 			connections[randRoom][i] = 1;
-
-			// append to the files each other connection
-			int connectionNum;
-			connectionNum = j + 1;
-			temp = fopen(filepaths[i], "a");
-			fprintf(temp,"CONNECTION %d: %s\n", connectionNum, selectedNames[randRoom]);
-			fclose(temp);
+			curConn++;
 		}
 		
+	}
+
+	// Now that the matrix of connections has been made, addpend the connections to the files
+	for(i =0; i < 7; i++) {
+		int counter = 1;
+		temp = fopen(filepaths[i], "a");
+		for(j = 0; j < 7; j++) {
+			if(connections[i][j] == 1 && (i != j)) {
+				fprintf(temp,"CONNECTION %d: %s\n", counter, selectedNames[j]);
+				counter++;
+			}
+		}
+		fclose(temp);
 	}
 
 }
 
 
 void createTypes() {
-	
+	int i;
+	FILE* temp;
+	for(i = 0; i < 7; i++) {
+		temp = fopen(filepaths[i], "a");
+		if(i == 0) {
+			fprintf(temp, "ROOM TYPE: %s\n", roomtypenames[0]);
+		}
+		else if(i == 6) {
+			fprintf(temp, "ROOM TYPE: %s\n", roomtypenames[2]);
+		}
+		else {
+			fprintf(temp, "ROOM TYPE: %s\n", roomtypenames[1]);
+		}
+		
+	}
 }
 
 // This helper function only serves to pick the 7 random rooms
