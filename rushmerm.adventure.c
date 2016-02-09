ac5b4efe49c6f0426d3790c5c6e8cbb6ab0cd32f
Mark Rushmere <mrushmere@gmail.com>
2016-02-08 19:57:14 -0800

    final version

--- rushmerm.adventure.c
+++ rushmerm.adventure.c
@@ -1,3 +1,13 @@
+/******************************************************
+* Mark Rushmere
+* CS 362
+* Project 2
+* Description: This is a text based game that requires
+* the user to guess rooms that have been randomly 
+* picked and connected until they guess the end room.
+******************************************************/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -13,9 +23,9 @@ char* filepaths[7];
 char* selectedNames[7];
 int connections[7][7] = {0};
 char* roomtypenames[3] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
-char* names[10] = {"WhiteHouse", "Cellar", "Cave", "TrollRoom", "TreasureRoom", "Room6", "Room7", "Room8", "Room9", "Room10"};
 
-
+// Room names from the game ZORK
+char* names[10] = {"WhiteHouse", "Cellar", "Cave", "TrollRoom", "TreasureRoom", "Forest", "WizardRoom", "CarouselRoom", "CoalMine", "RiddleRoom"};
 
 // Function Prototypes
 void pickRooms(int array[]);
@@ -23,10 +33,10 @@ void createRooms();
 void createConnections();
 void createTypes();
 void game();
+void cleanup();
 
 
 // main function
-
 int main() {
 	int i, j;
 
@@ -34,26 +44,13 @@ int main() {
 
 	createRooms();
 
-	for(i = 0; i < 7; i++) {
-		printf("%d: %s\n", i, selectedNames[i]);
-	}
-
-
 	createConnections();
-
-	printf("\n");
-	for(i = 0; i < 7; i++) {
-		for(j = 0; j < 7; j++) {
-			printf("%d\t", connections[i][j]);
-		}
-		printf("\n");
-		
-	}
-
 
 	createTypes();
 
 	game();
+
+	cleanup();
 
 	return 0;
 }
@@ -64,22 +61,12 @@ void createRooms() {
 
 	FILE* roomFiles[10];
 
-	// Room types
-	enum roomtype {START_ROOM, MID_ROOM, END_ROOM};
-	
-
-	// room names
 
 	// Array of 7 differnt rooms to be created
 	int selectedRooms[7] = {-1};
 
-	// Pick rooms
+	// Pick rooms using helper function
 	pickRooms(selectedRooms);
-	int n;
-	for(n = 0; n < 7; n++){
-		printf("%d\n", selectedRooms[n]);
-	}
-
 
 	// Get the process id and make the directory string
 	int processId;
@@ -89,7 +76,6 @@ void createRooms() {
 	char dir[40];
 	strcpy(dir, "./rushmerm.rooms.");	
 	strcat(dir, buffer);
-	printf("directory: %s\n", dir);
 	// Make the directory that all the room files will be in
 	mkdir(dir, 0777);
 
@@ -107,17 +93,14 @@ void createRooms() {
 		selectedNames[i] = (char*) malloc(20);
 	}
 
+	// Construct the full filepaths for each of the rooms
 	for(i =0; i < 7; i++) {
 		strcpy(filepaths[i], dir);
 		strcat(filepaths[i], "/");
 		strcat(filepaths[i], names[selectedRooms[i]]);
 		strcpy(selectedNames[i], names[selectedRooms[i]]);
 		strcat(filepaths[i], ".txt");
-		printf("Full file path: %s\n", filepaths[i]);
 	}
-	// Just checking that the room names and types work
-	printf("This is the first room name: %s\n", names[0]);
-	printf("This is the first room type: %s\n", roomtypenames[0]);
 
 	FILE* temp;
 
@@ -129,15 +112,6 @@ void createRooms() {
 		fprintf(temp,"ROOM NAME: %s\n", names[selectedRooms[i]]);
 		fclose(temp);
 	}
-
-	/****
-	for(i =0; i < 7; i++) {
-		free(filepaths[i]);
-	}
-	*****/
-
-
-
 }
 
 // This function is called to write the connections between the rooms
@@ -151,20 +125,9 @@ void createConnections() {
 	for(i = 0; i < 7; i++) {
 		connections[i][i] = 1;
 	}
-	for(i = 0; i < 7; i++) {
-		for(j = 0; j < 7; j++) {
-			printf("%d\t", connections[i][j]);
-		}
-		printf("\n");
-		
-	}
 
-
-
-	//printf("1");
 	// Now set the rest of the connections
 	for(i = 0; i < 7; i++) {
-		//printf("2");
 		// pick a random number between 3 and 6 to use for the number of connections for that room
 		int random;
 		random = (rand() % 4) + 3;
@@ -177,7 +140,6 @@ void createConnections() {
 
 		//make the connections, fill the matrix
 		while(curConn <= random) {
-			//printf("3");
 			// generate another random number to pick a different room to connect to
 			int randRoom;
 			randRoom = rand() % 7;
@@ -191,8 +153,7 @@ void createConnections() {
 			connections[i][randRoom] = 1;
 			connections[randRoom][i] = 1;
 			curConn++;
-		}
-		
+		}		
 	}
 
 	// Now that the matrix of connections has been made, addpend the connections to the files
@@ -227,15 +188,12 @@ void createTypes() {
 		else {
 			fprintf(temp, "ROOM TYPE: %s\n", roomtypenames[1]);
 		}
-		
+		fclose(temp);
 	}
 }
 
 // This helper function only serves to pick the 7 random rooms
 void pickRooms(int array[]) {
-
-
-
 	int i=0;
 	int j;
 	int randNum;
@@ -261,7 +219,7 @@ void game() {
 	// variables for gameplay
 
 	// Array to store user's path to final room
-	int i, j;
+	int i, j, sum, numPrinted;
 	int roomPath[50] = {-1};
 	bool foundEnd = false;
 	int numSteps = 0;
@@ -276,14 +234,28 @@ void game() {
 		while(!validUserSelection) {
 
 
+			// get number of connections for current room
+			sum = 0;
+			for(j = 0; j < 7; j++) {
+				if(connections[curRoom][j] == 1) {
+					sum++;
+				} 
+			}
+
+			numPrinted = 0;
+			sum--;
 			printf("CURRENT LOCATION: %s\n", selectedNames[curRoom]);
 			printf("POSSIBLE CONNECTIONS: ");
 			for(i = 0; i < 7; i++) {
 				if(connections[curRoom][i] == 1 && (i != curRoom)) {
-					printf("%s, ", selectedNames[i]);
+					printf("%s", selectedNames[i]);
+					numPrinted++;
+					if(numPrinted < sum) {
+						printf(", ");
+					}
 				}
 			}
-			printf("\n");
+			printf(".\n");
 			
 			char userSelection[25];
 			printf("WHERE TO? >");
@@ -292,7 +264,7 @@ void game() {
 
 			for(i = 0; i < 7; i ++) {
 				// Check that the name is valid and that there is a connection
-				if(strcmp(userSelection, selectedNames[i]) == 0 && connections[curRoom][i] == 1) {
+				if(strcmp(userSelection, selectedNames[i]) == 0 && connections[curRoom][i] == 1 && curRoom != i) {
 					validUserSelection = true;
 					curRoom = i;
 					roomPath[numSteps] = curRoom;
@@ -302,13 +274,13 @@ void game() {
 					}
 				}
 			}
+			// Message for if the user does not correctly enter an option
 			if(!validUserSelection) {
-				printf("HUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN\n\n");
+				printf("HUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN\n\n\n");
 			}
 			else {
 				printf("\n\n");
-			}
-			
+			}	
 		}
 	}
 	if(foundEnd) {
@@ -319,4 +291,13 @@ void game() {
 		}
 	}
 
+}
+
+// Free up memory allocated during the game
+void cleanup() {
+	int i;
+	for(i =0; i < 7; i++) {
+		free(filepaths[i]);
+		free(selectedNames[i]);
+	}
 }
